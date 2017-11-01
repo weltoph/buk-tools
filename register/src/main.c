6693d5b82a37d5edcd7d57619ad8dec48584d13f
Christoph Welzel <christoph.welzel@rwth-aachen.de>
2017-11-01 20:15:24 +0100

    implemented argument parsing and debug mode

--- register/src/main.c
+++ register/src/main.c
@@ -8,7 +8,7 @@
 #include "../../util/set.h"
 #include "prog.h"
 
-const char *argp_program_version = NULL;
+const char *argp_program_version = "unspecified";
 const char *argp_program_bug_address = "https://github.com/weltoph/buk-tools";
 
 typedef struct {
@@ -21,10 +21,10 @@ typedef struct {
 
 struct argp_option options[] = { 
   { .name = "debug", .key = 'd', .arg = NULL, .flags = 0,
-    .doc = "activates debug-mode which allows for close monitoring of execution",
-    .group = 0 },
+    .doc = "Enters debug-mode", .group = 0 },
   { .name = "arity", .key = 'a', .arg = "value", .flags = 0,
-    .doc = "indicates the arity of the computed function", .group = 0 }
+    .doc = "Defines arity of computed function", .group = 0 },
+  { 0 }
 };
 
 static error_t parse_opt(int key, char *arg, struct argp_state *state)
@@ -53,10 +53,104 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state)
 }
 
 struct argp argp = { options, parse_opt, "FILENAME [INPUTS]",
-  "simple simulator for register machines", 0, 0, 0 };
+  "simulator for register machines", 0, 0, 0 };
 
 extern FILE *yyin;
 extern Prog *parse_result;
+
+static void debug_show_reg(Prog *prog, char *input)
+{
+  uint8_t current_reg;
+  char *input_cpy = malloc(strlen(input) + 1);
+  if(!input_cpy) {
+    fprintf(stderr, "DEBUG-ERROR: could not allocate memory for copy of input %s\n",
+        input);
+    return;
+  }
+  strcpy(input_cpy, input);
+  char *current = input_cpy;
+  char *token = NULL;
+  /* skip first token */
+  token = strsep(&current, " ");
+  for(token = strsep(&current, " "); token != NULL; token = strsep(&current, " ")) {
+    current_reg = (uint8_t)atoi(token);
+    print_registers(prog, current_reg, current_reg);
+  }
+  free(input_cpy);
+}
+
+static void debug_access_reg(Prog *prog, char *input)
+{
+  uint8_t arg1;
+  uint8_t arg2;
+  char *input_cpy = malloc(strlen(input) + 1);
+  if(!input_cpy) {
+    fprintf(stderr, "DEBUG-ERROR: could not allocate memory for copy of input %s\n",
+        input);
+    return;
+  }
+  strcpy(input_cpy, input);
+  char *current = input_cpy;
+  char *token = NULL;
+  /* skip first token */
+  token = strsep(&current, " ");
+  token = strsep(&current, " ");
+  if(!token) {
+    fprintf(stderr, "DEBUG-ERROR: could not extract first argument of input %s\n",
+        input);
+    free(input_cpy);
+    return;
+  }
+  arg1 = (uint8_t)atoi(token);
+  if(!current) {
+    fprintf(stderr, "DEBUG-ERROR: could not extract second argument of input %s\n",
+        input);
+    free(input_cpy);
+    return;
+  }
+  arg2 = (uint8_t)atoi(current);
+  set_reg(prog, arg1, arg2);
+  free(input_cpy);
+}
+
+static void debug_prog(Prog *prog)
+{
+  while(!is_finished(prog)) {
+    cmd_fprint(stdout, prog->current);
+    fprintf(stdout, "\n");
+
+    fprintf(stdout, "options:\n");
+    fprintf(stdout, "\t[n] : executing next step (default)\n");
+    fprintf(stdout, "\t[r] : run program to the end\n");
+    fprintf(stdout, "\t[a] n m : setting register n to value m\n");
+    fprintf(stdout, "\t[s] n1 ... nk : printing registers n1 to nk\n");
+
+    char *input = NULL;
+    size_t buf_size = 0;
+    ssize_t input_len = 0;
+    input_len = getline(&input, &buf_size, stdin);
+    if(input_len < 0 || buf_size == 0) {
+      fprintf(stderr, "DEBUG-ERROR: could not get input\n");
+      return;
+    }
+    if(input_len == 1) {
+      step(prog);
+    } else {
+      switch(input[0]) {
+        case 'n': step(prog);
+                  break;
+        case 'r': exec(prog);
+                  break;
+        case 's': debug_show_reg(prog, input);
+                  break;
+        case 'a': debug_access_reg(prog, input);
+                  break;
+      }
+    }
+    free(input);
+  }
+  fprintf(stdout, "Program terminated...\n");
+}
 
 int main(int argc, char *argv[])
 {
@@ -99,15 +193,7 @@ int main(int argc, char *argv[])
   }
 
   if(args.debug) {
-    while(!is_finished(prog)) {
-      cmd_fprint(stdout, prog->current);
-      fprintf(stdout, "\n");
-      fprintf(stdout, "options:\n\t[n] : executing next step\n\t");
-      fprintf(stdout, "[r] : run program to the end\n\t");
-      fprintf(stdout, "[s] n m : printing registers n to m\n\t");
-      fprintf(stdout, "[a] n m : setting register n to value m\n");
-      step(prog);
-    }
+    debug_prog(prog);
   } else {
     exec(prog);
   }
